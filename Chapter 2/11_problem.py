c6b3fd0070d5f8c362514a366edec9e5d16efedd
Amit Singh <amitsingh100014@gmail.com>
2026-02-12 21:14:27 +0530

    Basic problem

---
+++ Chapter 2/11_problem.py
@@ -0,0 +1,6 @@
+# This program calculates the average of two numbers entered by the user and prints the result. 
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+
+print("The average of these two number is", (a+b)/2)
