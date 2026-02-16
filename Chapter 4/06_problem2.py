d50ef60e13e7ac16da798ca2a99899c857c56442
Amit Singh <amitsingh100014@gmail.com>
2026-02-16 21:35:21 +0530

    example of list

---
+++ Chapter 4/06_problem2.py
@@ -0,0 +1,20 @@
+# Write a program to take 6 subject marks from user and store them in a list. Sort the list and print it.
+
+marks = []
+
+f1 = int(input("Enter Marks here: "))
+marks.append(f1)
+f2 = int(input("Enter Marks here: "))
+marks.append(f2)
+f3 = int(input("Enter Marks here: "))
+marks.append(f3)
+f4 = int(input("Enter Marks here: "))
+marks.append(f4)
+f5 = int(input("Enter Marks here: "))
+marks.append(f5)
+f6 = int(input("Enter Marks here: "))
+marks.append(f6)
+
+marks.sort()
+
+print(marks)
